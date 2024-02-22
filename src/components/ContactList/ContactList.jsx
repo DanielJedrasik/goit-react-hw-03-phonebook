c762507e4c2f9859d21ebad7f319b5b66c6d80af
DanielJedrasik <djedrasik3@gmail.com>
2024-02-22 16:29:31 +0100

    Complete phonebook

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,42 @@
+import PropTypes from 'prop-types';
+import css from './ContactList.module.css';
+
+const MyContactList = ({ contacts, removeContact }) => {
+  if (!contacts) {
+    return null;
+  }
+
+  const names = contacts.map(({ id, name, number }) => (
+    <li className={css.list_item} key={id}>
+      <p>{name}</p>
+      <p>:</p>
+      <p>{number}</p>
+      <button
+        className={css.button}
+        onClick={() => removeContact(id)}
+        type="button"
+      >
+        Delete
+      </button>
+    </li>
+  ));
+
+  return (
+    <div className={css.contacts_wraper}>
+      <ul className={css.contact_items}>{names}</ul>
+    </div>
+  );
+};
+
+export default MyContactList;
+
+MyContactList.propTypes = {
+  removeContact: PropTypes.func.isRequired,
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
+};
